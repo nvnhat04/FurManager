7bcf225f6d199b7a15ab9260be1791ef5d85a666
nvnhat04 <nvnhat04324@gmail.com>
2025-07-26 22:57:43 +0700

    feat: create HomeScreen

--- app/src/main/java/com/example/furmanager/ui/HomeScreen.kt
+++ app/src/main/java/com/example/furmanager/ui/HomeScreen.kt
@@ -1,45 +1,101 @@
 package com.example.furmanager.ui
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.furmanager.R
+import com.example.furmanager.ui.components.AutoSlidingBanner
+import com.example.furmanager.ui.components.BannerData
+import com.example.furmanager.ui.components.Header
+import com.example.furmanager.ui.components.Product
+import com.example.furmanager.ui.components.ProductGrid
+import com.example.furmanager.ui.components.SearchBar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 @Composable
 fun HomeScreen(viewModel: AuthViewModel, navController: NavController) {
     val user = FirebaseAuth.getInstance().currentUser
-    val sampleProducts = listOf(
-        Product("Nike", "Air Max 90", "$129.99", R.drawable.sofa),
-        Product("Adidas", "Ultraboost", "$149.99", R.drawable.sofa),
-        Product("Puma", "RS-X", "$109.99",R.drawable.sofa)
-    )
+    val uid = user?.uid ?: return
 
-
-    Column(
-        modifier = Modifier.padding(8.dp),
-
-    ){
-        Text("Xin chào, ${user?.email}")
-        ProductSlider(products = sampleProducts)
-        Button(onClick = {
-            viewModel.signOut {
-                navController.navigate("login") {
-                    // Xóa backstack để không quay lại màn Home
-                    popUpTo("home") { inclusive = true }
+    val nameState = remember { mutableStateOf("Loading...") }
+    val query = remember { mutableStateOf("") }
+    // Firestore gọi bên trong SideEffect để tránh gọi nhiều lần
+    LaunchedEffect(uid) {
+        val db = FirebaseFirestore.getInstance()
+        db.collection("users").document(uid)
+            .get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    nameState.value = document.getString("name") ?: "No name"
                 }
             }
-        }) {
-            Text("Sign Out")
+            .addOnFailureListener {
+                nameState.value = "Error"
+            }
+    }
+
+    val sampleProducts = listOf(
+        Product("Air Max 90", "$129.99", R.drawable.sofa),
+        Product("Ultraboost", "$149.99", R.drawable.sofa),
+        Product("RS-X", "$109.99", R.drawable.sofa),
+        Product("Air Max 90", "$129.99", R.drawable.sofa),
+        Product("Ultraboost", "$149.99", R.drawable.sofa),
+        Product("RS-X", "$109.99", R.drawable.sofa)
+    )
+    val banners = listOf(
+        BannerData("Banner 1", R.drawable.sofa),
+        BannerData("Banner 2", R.drawable.table),
+        BannerData("Banner 3", R.drawable.sofa)
+    )
+
+    Column(modifier = Modifier.padding(8.dp)) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Welcome to FurManager, ${nameState.value}",
+                fontWeight = FontWeight.SemiBold,
+                color = Color(200, 155, 255),
+                fontSize = 16.sp,
+                fontFamily = FontFamily.SansSerif
+            )
         }
+        // searcg bar
+        SearchBar(
+            query = query.value,
+            onQueryChange = { query.value = it }
+        )
+
+
+        AutoSlidingBanner(banners = banners)
+        Header("Top monthly")
+        ProductSlider(products = sampleProducts, navController= navController)
+        Header("Recently")
+        ProductGrid(products = sampleProducts, navController = navController)
+
 
     }
 }
